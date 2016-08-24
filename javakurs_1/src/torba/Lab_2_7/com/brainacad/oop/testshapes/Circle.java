9a672a8f90d7b64179118bc261badc28d4a2c3fa
Aleksandr Torba <satir_ast@ukr.net>
2016-08-24 19:49:00 +0300

    Lab_2_7. Variant № 1.

--- javakurs_1/src/torba/Lab_2_7/com/brainacad/oop/testshapes/Circle.java
+++ javakurs_1/src/torba/Lab_2_7/com/brainacad/oop/testshapes/Circle.java
@@ -2,11 +2,14 @@ package torba.Lab_2_7.com.brainacad.oop.testshapes;
 
 public class Circle extends Shape {
 
+    static double area = 0;
+
     private double radius;
 
     public Circle(String color, double radius){
         setShapeColor(color);   //TODO use super constructor
         this.radius = radius;
+        area = area + calcArea();
     }
         //TODO use @Override
     public String toString(){
