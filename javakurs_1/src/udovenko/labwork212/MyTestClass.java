263089b22407aa0641fe20cd7c23242c5b11fad6
Udovenko Vasyl <gladius76@ukr.net>
2016-09-14 13:03:57 +0300

    Lab Work 2.12

---
+++ javakurs_1/src/udovenko/labwork212/MyTestClass.java
@@ -0,0 +1,13 @@
+package udovenko.labwork212;
+
+/**
+ * Created by gladi on 14.09.2016.
+ */
+class MyTestClass {
+    void test(){};
+    static class MyStaticNested{};
+    class Inner{};
+    void local(){
+        class MyLocal{};
+    }
+}
