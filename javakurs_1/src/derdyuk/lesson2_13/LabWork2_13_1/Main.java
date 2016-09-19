f8e0a7cd09f28ccedfdc9aa9ed3f98f53cdbb744
Derdyuk Sergey <derdyuk85@mail.ru>
2016-09-19 18:26:18 +0300

    lab 2.12 Derdyuk

---
+++ javakurs_1/src/derdyuk/lesson2_13/LabWork2_13_1/Main.java
@@ -0,0 +1,12 @@
+package derdyuk.lesson2_13.LabWork2_13_1;
+
+/**
+ * Created by maossi on 12.09.2016.
+ */
+public class Main {
+    public static void main(String[]args){
+        for(MyDayOfWeek myday:MyDayOfWeek.values()){
+            System.out.println(myday);
+        }
+    }
+}
