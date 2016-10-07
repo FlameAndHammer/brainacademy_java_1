251d5e76980664165de5863642a5abd9a6c924af
Nikulin Alexander <shas@email.ua>
2016-10-07 18:50:00 +0300

    practice task 3

---
+++ javakurs_1/src/_nikulin/practice/threads/task3/Test.java
@@ -0,0 +1,38 @@
+package _nikulin.practice.threads.task3;
+
+/**
+ * Created by Александр on 07.10.2016.
+ * Исправить ошибки
+ */
+/*class BoxPrinter {
+    private Object val;
+
+    public BoxPrinter(Object arg) {
+        val = arg;
+    }
+
+    public String toString() {
+        return "{" + val + "}";
+    }
+
+    public Object getValue() {
+        return val;
+    }
+
+    public static <T extends BoxPrinter> T returnGenericNewInstance(T someT){
+        return Class.forName(T).newInstance();
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        BoxPrinter value1 = new BoxPrinter(new Integer(10));
+        System.out.println(value1);
+        Integer intValue1 = (Integer) value1.getValue();
+        BoxPrinter value2 = new BoxPrinter("Hello world");
+        System.out.println(value2);
+
+        Integer intValue2 = (Integer) value2.getValue();
+        System.out.println(BoxPrinter.returnGenericNewInstance(value1));
+    }
+}*/
