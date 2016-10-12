23fac62ade8e2a756761dbe35920be192f38e4e9
Udovenko Vasyl <gladius76@ukr.net>
2016-10-12 16:46:54 +0300

    task3 ???????????

---
+++ javakurs_1/src/udovenko/labwork217/testthread8/BankTest.java
@@ -0,0 +1,15 @@
+package udovenko.labwork217.testthread8;
+
+/**
+ * Created by gladi on 30.09.2016.
+ */
+public class BankTest {
+    public static final int N_ACCOUNTS = 5;
+    public static final int INIT_BALANCE = 1000;
+
+    public static void main(String args[]) {
+        //...
+        // COMPLETE
+        //..
+    }
+}
