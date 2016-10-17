9e0d1962094755c5dbc5b75732a9bd4ab6fa39e8
Nikulin Alexander <shas@email.ua>
2016-10-17 20:56:32 +0300

    task 1

---
+++ javakurs_1/src/_nikulin/labs/lesson_3_1/task1/Main.java
@@ -0,0 +1,19 @@
+package _nikulin.labs.lesson_3_1.task1;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Created by Александр on 17.10.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        File file = null;
+        if (args.length > 0){
+            file = new File(args[0]);
+        } else {
+            file = new File("./javakurs_1/src/_nikulin/labs");
+        }
+        System.out.println(Arrays.toString(file.list()));
+    }
+}
