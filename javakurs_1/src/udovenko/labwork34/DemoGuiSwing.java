8ecc9c53c5c36e02d88ac459b308542de51d6d7f
Udovenko Vasyl <gladius76@ukr.net>
2016-11-02 21:01:49 +0200

    LabWork 3-4-1

---
+++ javakurs_1/src/udovenko/labwork34/DemoGuiSwing.java
@@ -0,0 +1,10 @@
+package udovenko.labwork34;
+
+/**
+ * Created by gladi on 02.11.2016.
+ */
+public class DemoGuiSwing {
+    public static void main(String[] args) {
+        new MyApplication("Yessss!!!!!!!");
+    }
+}
---
+++ javakurs_1/src/udovenko/labwork34/MyApplication.java
@@ -0,0 +1,16 @@
+package udovenko.labwork34;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by gladi on 02.11.2016.
+ */
+class MyApplication extends JFrame {
+    public MyApplication(String title) throws HeadlessException {
+        super(title);
+        setSize(300, 200);
+        setLocation(100,100);
+        setVisible(true);
+    }
+}
