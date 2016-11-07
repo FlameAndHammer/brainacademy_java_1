bb7cc57d4f8e454e969801e7648185c7d09552a7
Aleksandr Torba <satir_ast@ukr.net>
2016-11-07 16:08:30 +0200

    Delete spring

--- javakurs_1/src/torba/spring/src/main/java/spring/solutions/task1/formula1/marshal/Marshal.java
+++
@@ -1,9 +0,0 @@
-package spring.solutions.task1.formula1.marshal;
-
-/**
-
- */
-public interface Marshal {
-
-    void showFlag();
-}
--- javakurs_1/src/torba/spring/src/main/java/spring/templates/formula1/marshal/Marshal.java
+++
@@ -1,9 +0,0 @@
-package spring.templates.formula1.marshal;
-
-/**
-
- */
-public interface Marshal {
-
-    void showFlag();
-}
